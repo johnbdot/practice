dd25a89fed8a50dc2ce704c28cf62ca4c797489a
John Bouakhasith <johnbeedot@gmail.com>
2021-05-16 14:35:00 -0400

    none

--- treehouse/javascript-objects/2 - Loop Through Objects/4 - Build an Object Challenge/start/js/pets.js
+++ treehouse/javascript-objects/2 - Loop Through Objects/4 - Build an Object Challenge/start/js/pets.js
@@ -3,3 +3,57 @@
   Each object should have the following properties: 
   name, type, breed, age, and photo
 */
+
+const pets = [
+  {
+    name: 'Mylo',
+    type: 'Dog',
+    breed: 'Aussie',
+    age: 1,
+    photo: 'img/aussie.jpg',
+    alt: 'Austrailian Shepherd'
+  },
+  {
+    name: 'Ben',
+    type: 'Dog',
+    breed: 'Dachshund',
+    age: 3,
+    photo: 'img/dachshund.jpg'
+  },
+  {
+    name: 'Bob',
+    type: 'Dog',
+    breed: 'Golden',
+    age: 5,
+    photo: 'img/golden.jpg'
+  },
+  {
+    name: 'Tom',
+    type: 'Cat',
+    breed: 'Persian',
+    age: 2,
+    photo: 'img/persian.jpg'
+  },
+  {
+    name: 'Kevin',
+    type: 'Cat',
+    breed: 'Tabby',
+    age: 4,
+    photo: 'img/tabby.jpg'
+  }
+]
+
+function createListItems(arr) {
+  let items = '';
+  for (let pet in arr) {
+    items += `
+    <h2>${arr[pet].name}</h2>
+    <h3>${arr[pet].type} | ${arr[pet].breed}</h3>
+    <p>Age: ${arr[pet].age}</p>
+    <img src="${arr[pet].photo}" alt="${arr[pet].alt}">
+    `;
+  }
+  return items;
+}
+
+document.querySelector('main').innerHTML = createListItems(pets);
