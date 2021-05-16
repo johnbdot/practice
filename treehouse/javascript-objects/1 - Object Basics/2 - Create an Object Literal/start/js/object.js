7e0d8e6ea08dcb9549568cf93799d886f93498cc
John Bouakhasith <johnbeedot@gmail.com>
2021-05-16 12:49:57 -0400

    added nickname object

--- treehouse/javascript-objects/1 - Object Basics/2 - Create an Object Literal/start/js/object.js
+++ treehouse/javascript-objects/1 - Object Basics/2 - Create an Object Literal/start/js/object.js
@@ -3,5 +3,9 @@ const person = {
     city: 'New York',
     age: 37,
     isStrudent: true,
-    skills: ['JacaScript', 'HTML', 'CSS']
+    skills: ['JavaScript', 'HTML', 'CSS']
 };
+
+person.nickname = 'Duke';
+const message = `Hi, I'm ${person.name}. I live in ${person.city}. Most know me as ${person.nickname = 'Duke'}. My age is ${person.age + 1}. I have ${person.skills.length} skills: ${person.skills.join(', ')}`;
+console.log(message)
