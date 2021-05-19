32589df6c910782fdfaa5ff6ed21b15b78a1d0d6
John Bouakhasith <johnbeedot@gmail.com>
2021-05-18 22:37:37 -0400

    modified

--- treehouse/3-making-changes-to-the-DOM/vid-4-creating-DOM-elements/start/app.js
+++ treehouse/3-making-changes-to-the-DOM/vid-4-creating-DOM-elements/start/app.js
@@ -3,18 +3,24 @@ const listDiv = document.querySelector('.list');
 const input = document.querySelector('input.description');
 const p = document.querySelector('p.description');
 const button = document.querySelector('button.description');
+const addItemInput = document.querySelector('input.addItemInput');
+const addItemButton = document.querySelector('button.addItemButton');
 
 toggleList.addEventListener('click', () => {
   if (listDiv.style.display == 'none') {
     toggleList.textContent = 'Hide list';
     listDiv.style.display = 'block';
   } else {
-    toggleList.textContent = 'Show list';                        
+    toggleList.textContent = 'Show list';
     listDiv.style.display = 'none';
-  }                         
+  }
 });
 
 button.addEventListener('click', () => {
   p.innerHTML = input.value + ':';
 });
 
+addItemButton.addEventListener('click', () => {
+  let li = document.createElement('li');
+  li.textContent = addItemInput.value;
+});
